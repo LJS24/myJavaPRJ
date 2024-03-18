70d544e4b32b1bbe8ac870e14bb355eccaea2496
LJS24 <ewepyon@gmail.com>
2024-03-18 14:09:07 +0900

    20240318 두 번째 파일

---
+++ src/main/java/org/example/Code02_06.java
@@ -0,0 +1,22 @@
+package org.example;
+/* 같은 타입의 변수는 쉽표로 한번에 가능.
+= 오른쪽이 먼저 다 계산된 후 왼쪽에 대입되는 것과
+Result와 같은 대입연산자가 무조건 왼쪽임을 기억할 것.
+ */
+public class Code02_06 {
+    public static void main(String[] args) {
+        int num1, num2, result1, result2, result3, result4 ;
+        num1 = 200;
+        num2 = 100;
+
+        result1 = num1 + num2;
+        result2 = num1 - num2;
+        result3 = num1 * num2;
+        result4 = num1 / num2;
+        System.out.println(num1 + " + " + num2 + " = " + result1);
+        System.out.println(num1 + " + " + num2 + " = " + result2);
+        System.out.println(num1 + " * " + num2 + " = " + result3);
+        System.out.println(num1 + " / " + num2 + " = " + result4);
+
+    }
+}
