d0437b3c6777cd51456a74c29373e9bbedf08c2a
LJS24 <ewepyon@gmail.com>
2024-03-26 13:12:35 +0900

    20240326 잔디심기_삼각형별찍기

---
+++ src/main/java/star_trinangle.java
@@ -0,0 +1,17 @@
+public class star_trinangle {
+        public static void main(String[] args) {
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j <(4+i); j++) {
+                    if (j<(3-i)) {
+
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("*");
+                    }
+                }
+                System.out.println();
+            }
+
+        }
+    }
+
