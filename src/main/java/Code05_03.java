f16229dfdd52fd65d933034f6cf9a22d2cb1a8ec
LJS24 <ewepyon@gmail.com>
2024-04-01 13:45:39 +0900

    20240326 두 번째_Scanner와 if

---
+++ src/main/java/Code05_03.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Code05_03 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int a;
+
+        System.out.println("정수를 입력하세요-->");
+        a = s.nextInt();
+        if (a<100) {
+            System.out.println("입력한 수가 100보다 작습니다");
+        }
+
+    s.close();
+    }
+}
