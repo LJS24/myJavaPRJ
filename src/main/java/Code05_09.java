b02f0735c5b4c121002bee1a562cb96026f458e4
LJS24 <ewepyon@gmail.com>
2024-04-01 15:16:43 +0900

    20240326 여덟 번쨰_switch case문

--- src/main/java/Code05_09.java
+++ src/main/java/Code05_09.java
@@ -18,10 +18,10 @@ public class Code05_09 {
 
             case 3:
                 System.out.print("3을 선택했습니다.");
-                break;
+                break;  /*이거 없으면 안 끝나서 모든 경우가 실행된다.*/
 
-            default:
-                System.out.print("잘못된 선택입니다.");
+            default:  /*else if문의 else와 같음.*/
+                System.out.print("잘못된 선택입니다.");  /*if else문은 하나 실행하고 다른 경우를 지나가는 방식이며, switch case문은 하나 실행하고 함수에서 빠져나가는 방식.*/
         }
     s.close();
     }
