266bb168839d47245b6755321b4da0b6de7458e6
LJS24 <ewepyon@gmail.com>
2024-04-01 13:36:15 +0900

    20240326 첫 번째_여러 줄 출력하는 if문

---
+++ src/main/java/Code05_0102.java
@@ -0,0 +1,14 @@
+public class Code05_0102 {
+    public static void main(String[] args) {
+        int num = 99;
+
+        if (num < 100) {
+            System.out.println("100보다");
+            System.out.println("작습니다");         /*if문은 {}가 없다면 그 아래 한 줄만 받고, 아래 줄은 따로 실행된다. */
+        }
+        System.out.println("난 무조건 실행된다.");
+
+
+
+    }
+}
