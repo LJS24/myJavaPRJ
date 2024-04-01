680744a4f903bdd249ec0f4eb75cb3954ea993f5
LJS24 <ewepyon@gmail.com>
2024-04-01 14:05:15 +0900

    20240326 두 번째_변형_Scanner와 if

--- src/main/java/Code05_02_1.java
+++ src/main/java/Code05_02_1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Code05_03 {
+public class Code05_02_1 {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int a;
