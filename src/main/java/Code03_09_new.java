07cbc7dc03f578b53539efee716ae1ee86829068
LJS24 <ewepyon@gmail.com>
2024-04-09 14:24:00 +0900

    20240409_수업 첫 번째 파일

---
+++ src/main/java/Code03_09_new.java
@@ -0,0 +1,11 @@
+public class Code03_09_new {
+    public static void main(String [] args){
+        int num=20;
+        System.out.println(++num + " : num 변수 출력 전에 +1이 되어서 21이 출력됨 ");
+        System.out.println(num++ + " : num이 출력되고 +1을 수행 ");
+        System.out.println(num + " : 22 출력");
+        System.out.println(--num + " : num 변수 출력 전에 -1이 되어서 21이 출력됨");
+        System.out.println(num-- + " : num이 출력되고 -1을 수행");
+        System.out.println(num + " : 20 출력");
+    }
+}
