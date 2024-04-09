61c5c205dad0ce95ff4d624291c8293530d1dc6b
LJS24 <ewepyon@gmail.com>
2024-04-09 14:46:58 +0900

    20240409_수업 퀴즈2

---
+++ src/main/java/Code06_quiz2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Code06_quiz2 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int a, b;
+        System.out.println("첫 번째 수를 입력:");
+        a = s.nextInt();
+        System.out.println("두 번째 수를 입력:");
+        b = s.nextInt();
+
+        if (a<b) {
+
+            for (int i = a; i < b + 1; i++) {
+                System.out.println(i);
+            }
+        }
+        else if (a>b) {
+            for (int i = a; i > b; i--) {
+                System.out.println(i);
+            }
+        }
+        s.close();
+    }
+}
