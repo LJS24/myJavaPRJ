c1afa7acdb2aba64f331e52e946fdb4ddbef97fc
LJS24 <ewepyon@gmail.com>
2024-04-15 14:19:33 +0900

    20240415_두번째파일_중첩for문_구구단출력

---
+++ src/main/java/Code06_10.java
@@ -0,0 +1,12 @@
+public class Code06_10 {
+    public static void main(String[] args) {
+
+
+
+        for (int i = 2; i <=9; i++) {
+            for (int f = 1; f <=9; f++) {
+                System.out.println(i + "X" + f + "=" + i*f);
+            }
+        }
+    }
+}
