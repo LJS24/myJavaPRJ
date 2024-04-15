c7f55ad0afe3ffcad102b396dffc5ec8f098d945
LJS24 <ewepyon@gmail.com>
2024-04-15 12:27:25 +0900

    20240412_퀴즈_범위내홀수합출력_보완

--- src/main/java/Personal_13.java
+++ src/main/java/Personal_13.java
@@ -8,11 +8,16 @@ public class Personal_13 {
         a = s.nextInt();
         System.out.println("300을 입력:");
         b = s.nextInt();
+        if (a !=200 || b !=300){
+            System.out.println("잘못된 입력입니다. 다시 시작하십시오.");
+        }
+        else {
 
-        for (int i = a; i < b + 1; i++) {
-            if (i%2 != 0) {
-                hap += i;
-                System.out.println(hap);
+            for (int i = a; i < b + 1; i++) {
+                if (i % 2 != 0) {
+                    hap += i;
+                    System.out.println(hap);
+                }
             }
         }
         s.close();
