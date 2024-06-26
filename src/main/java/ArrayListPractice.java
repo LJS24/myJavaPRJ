cc9a55fa6950a8f41ff917f9f78977024e54cc38
LJS24 <ewepyon@gmail.com>
2024-06-26 11:25:35 +0900

    20240626_잔디심기_복습

---
+++ src/main/java/ArrayListPractice.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListPractice {
+    public static void main(String[] args) {
+        List<String> fruit = new ArrayList<String>();
+
+        fruit.add("apple");
+        fruit.add("banana");
+        fruit.add("melon");
+        fruit.add("lemon");
+
+        int size = fruit.size();
+
+        for(int i = 0; i < size; i++) {
+            String str = fruit.get(i);
+            System.out.println(i + " : " + str);
+        }
+
+        fruit.remove(0);
+    }
+}
