d61b517fda64f8323b84812a20ca32c2ac03a999
LJS24 <ewepyon@gmail.com>
2024-07-12 16:53:40 +0900

    20240712_잔디심기_복습

---
+++ src/main/java/dohwaji.java
@@ -0,0 +1,32 @@
+
+class Parent3 {
+    public int cal(int a, int b) {
+        return a + b;
+    }
+}
+
+// Child 클래스
+class Child2 extends Parent3 {
+    @Override
+    public int cal(int a, int b) {
+        // 예시로 곱셈으로 재정의합니다.
+        return a * b;
+    }
+}
+
+public class dohwaji{
+    public static void main(String[] args) {
+
+        Parent3 parent = new Parent3();
+        Child2 child = new Child2();
+
+        int parentResult = parent.cal(3, 4);
+        int childResult = child.cal(3, 4);
+
+        System.out.println("Parent result: " + parentResult);
+        System.out.println("Child result: " + childResult);
+    }
+}
+
+
+
